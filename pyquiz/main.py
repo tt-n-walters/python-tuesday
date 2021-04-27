2fff5eaf827d4d340f578d6817ba274311667eb7
nw <nico.walters@techtalents.es>
2021-04-27 20:16:18 +0200

    Added input validation to url choose functions

--- pyquiz/main.py
+++ pyquiz/main.py
@@ -7,7 +7,17 @@ class URL:
     def choose_number_of_questions(self):
         print("Enter number of questions:")
         self.number_of = input(">> ")
-
+        if self.number_of.isdigit():
+            self.number_of = int(self.number_of)
+            if self.number_of > 0:
+                print("Number of questions saved.")
+            else:
+                print("At least 1 question required.")
+                exit()
+        else:
+            print("Invalid input.")
+            exit()
+        
     def choose_category(self):
         print("Enter category:")
         self.category = input(">> ")
@@ -16,16 +26,22 @@ class URL:
         print("Enter difficulty:  (any/easy/medium/hard)")
         self.difficulty = input(">> ")
         if self.difficulty in ["any", "easy", "medium", "hard"]:
-            print
+            print("Difficulty saved.")
         else:
             print("Invalid difficulty.")
+            exit()
 
     def choose_type(self):
         print("Enter type of questions:  (any/multiple/boolean)")
         self.type = input(">> ")
+        if self.type in ["any", "multiple", "boolean"]:
+            print("Type saved.")
+        else:
+            print("Invalid type.")
+            exit()
     
     def generate(self):
-        pass
+        generated_url = "https://opentdb.com/api.php"
 
 
 new_url = URL()
